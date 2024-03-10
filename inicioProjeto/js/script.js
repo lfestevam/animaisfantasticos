9ba6e1b7f599ca71aa557ac1bcbb362a67eb1629
Luís Estevam <luisfestevam01@gmail.com>
2024-03-10 02:19:24 -0300

    atualizações
    separação  do JS em módulos, separação do CSS, e implementação do modal de login.

---
+++ inicioProjeto/js/script.js
@@ -0,0 +1,11 @@
+//Navegação por Tab
+import initTabNav from "./modules/navegacaoTab.js";
+initTabNav();
+//Acordeon List FAQ
+import initAccordion from "./modules/accordion.js";
+initAccordion();
+//Animação ao Scroll
+import initAnimaScroll from "./modules/animacaoScroll.js";
+initAnimaScroll();
+import initModal from "./modules/modal.js";
+initModal();
